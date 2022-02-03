eb09ea30e7a91b4591ddf9a5f42612d1f7762670
Adam Spannbauer <SpannbauerAdam@gmail.com>
2022-02-03 07:45:12 -0500

    add data read/prep code to server

--- server.R
+++ server.R
@@ -1,4 +1,9 @@
 library(shiny)
+library(fpp3)
+
+sales_df <- read.csv("data/clean_sales.csv")
+sales_df$date <- as.Date(sales_df$date)
+sales_df <- as_tsibble(sales_df, key = c("scode", "pcode"), index = "date")
 
 server <- function(input, output) {
 
